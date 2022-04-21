c9a4a52e07db4d818a0e5dc2040412966b4096b4
Maciej <kanabaj.maciej@gmail.com>
2022-04-21 23:01:44 +0200

    Maked module to record mouse moves

---
+++ recorder.py
@@ -0,0 +1,26 @@
+from pynput.mouse import Listener as MouseListener
+from time import sleep
+
+class MouseRecorder:
+    def __init__(self) -> None:
+        self._moves = []
+        self._listener = MouseListener(on_move= lambda x ,y : self._moves.append((x, y)))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._listener.stop()
+
+    def record(self, run_time = 0):
+        self._listener.start()
+        sleep(run_time)
+    
+    def save(self, output_file = 'out.txt'):
+        with open(output_file, 'a', encoding='utf-8') as file:
+            file.write(str(self._moves))
+
+if __name__ == '__main__':
+    with MouseRecorder() as recorder:
+        recorder.record(15)
+        recorder.save()
